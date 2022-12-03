5a437922c812b5d5423847f8847ca4c2605226f5
Neotwalker <and1marat9@gmail.com>
2022-12-03 19:22:46 +0500

    add

---
+++ js/common.js
@@ -0,0 +1,29 @@
+$(function() {
+
+	$("#phone_1").mask("+7 (999) 999-9999");
+
+	$('.info_1').hover( function(){
+		$('.info-1').toggleClass('active')
+	});
+	$('.info_2').hover( function(){
+		$('.info-2').toggleClass('active')
+	});
+	$('.info_3').hover( function(){
+		$('.info-3').toggleClass('active')
+	});
+	$('.info_4').hover( function(){
+		$('.info-4').toggleClass('active')
+	});
+	$('.info_5').hover( function(){
+		$('.info-5').toggleClass('active')
+	});
+	$('.info_6').hover( function(){
+		$('.info-6').toggleClass('active')
+	});
+	$('.info_7').hover( function(){
+		$('.info-7').toggleClass('active')
+	});
+	$('.info_8').hover( function(){
+		$('.info-8').toggleClass('active')
+	});
+});
